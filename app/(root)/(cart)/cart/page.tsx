fe6692a9386b970c79701a5411364742381854e6
Bhavik Maheta <18it.bhavik.maheta@gmail.com>
2023-10-16 15:48:36 +0530

    add image in cart page

--- app/(root)/(cart)/cart/page.tsx
+++ app/(root)/(cart)/cart/page.tsx
@@ -4,6 +4,7 @@ import { useCart } from "@/context/CartContext";
 import { Button } from "@/components/ui/button";
 import { Minus, Plus, Trash } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 
 const CartPage = () => {
   const { cart, updateQuantity, removeProduct } = useCart();
@@ -21,52 +22,41 @@ const CartPage = () => {
 
       {cart.length === 0 ? (
         <>
- <Link href={'/products'}>
-          <Button variant={'ghost'}>
-            Shop Now
-          </Button>
-
-        </Link>
-        
-        <p>Your cart is empty.</p>
+          <Link href={'/products'}>
+            <Button variant={'ghost'}>
+              Shop Now
+            </Button>
+          </Link>
+          <p>Your cart is empty.</p>
         </>
-       
       ) : (
         <div>
           {cart.map((product) => (
-            <div
-              key={product.id}
-              className="flex items-center justify-between border-b-2 p-4"
-            >
-              <div>
-                <h2 className="font-semibold text-xl">{product.title}</h2>
-                <p>{product.description}</p>
-                <p>Price: ${product.price}</p>
+            <div key={product.id} className="flex items-center justify-between border-b-2 p-4">
+              <div className="flex items-center">
+                <Image src={product.image} alt={product.title} className="h-16 w-16 mr-4" />
+                <div>
+                  <h2 className="font-semibold text-xl">{product.title}</h2>
+                  <p>{product.description}</p>
+                  <p>Price: ${product.price}</p>
+                </div>
               </div>
               <div className="flex items-center">
                 <Button
-                  onClick={() =>
-                    updateQuantity(product.id, (product.quantity || 1) + 1)
-                  }
+                  onClick={() => updateQuantity(product.id, (product.quantity || 1) + 1)}
                   className="bg-blue-500 text-white p-2 mx-2"
                 >
                   <Plus className="h-4 w-4 mr-2" />
                 </Button>
                 <p>Quantity: {product.quantity || 1}</p>
                 <Button
-                  onClick={() =>
-                    updateQuantity(product.id, (product.quantity || 1) - 1)
-                  }
+                  onClick={() => updateQuantity(product.id, (product.quantity || 1) - 1)}
                   className="bg-red-500 text-white p-2 mx-2"
                 >
-                  <Minus  className="h-4 w-4 mr-2" />
+                  <Minus className="h-4 w-4 mr-2" />
                 </Button>
-                <Button
-                  onClick={() => removeProduct(product.id)}
-                  className="bg-red-500 text-white p-2 mx-2"
-                >
-                                    <Trash  className="h-4 w-4 mr-2" />
-
+                <Button onClick={() => removeProduct(product.id)} className="bg-red-500 text-white p-2 mx-2">
+                  <Trash className="h-4 w-4 mr-2" />
                 </Button>
               </div>
             </div>
